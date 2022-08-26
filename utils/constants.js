68632ec0b8e51ed19b2696995976df03baaf514e
“David <daveh@mailbox.org>
2022-08-26 22:00:58 +0100

    Fixed lint issue

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,3 @@
-const maintainerTwitterURL = 'https://twitter.com/davunt_';
-
 module.exports = {
   appRepoURL: 'https://github.com/davunt/app-eraser',
   appTwitterURL: 'https://twitter.com/AppEraser',
