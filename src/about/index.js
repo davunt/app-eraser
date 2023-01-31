26e28e39a5348309d3e027b30355eeafe4c57aae
David Hunt <david@Davids-MacBook-Pro.local>
2023-01-31 22:41:57 +0000

    Updated about page

--- src/about/index.js
+++ src/about/index.js
@@ -1,10 +1,20 @@
 const { ipcRenderer } = require('electron');
-const { appRepoURL } = require('../../utils/constants');
+const { appRepoURL, appMastodonURL, maintainerMastodonURL } = require('../../utils/constants');
 
 const appRepoIcon = document.getElementById('appRepoIcon');
+const appMastoIcon = document.getElementById('appMastoIcon');
+const maintainerMastoIcon = document.getElementById('maintainerMastoIcon');
 
 appRepoIcon.addEventListener('click', () => {
   ipcRenderer.send('openURL', appRepoURL);
+});
+
+appMastoIcon.addEventListener('click', () => {
+  ipcRenderer.send('openURL', appMastodonURL);
+});
+
+maintainerMastoIcon.addEventListener('click', () => {
+  ipcRenderer.send('openURL', maintainerMastodonURL);
 });
 
 ipcRenderer.on('appVersion', (e, value) => {
--- utils/constants.js
+++ utils/constants.js
@@ -2,4 +2,6 @@ module.exports = {
   appRepoURL: 'https://github.com/davunt/app-eraser',
   issuesURL: 'https://github.com/davunt/app-eraser/issues',
   releasesURL: 'https://github.com/davunt/app-eraser/releases',
+  appMastodonURL: 'https://mastodon.ie/@davunt',
+  maintainerMastodonURL: 'https://mastodon.ie/@davunt',
 };
